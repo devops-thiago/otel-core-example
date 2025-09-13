162ccbac804c3cc89f111b18828d2d3862d39508
Thiago Gonzaga <thiago.gonzaga@icloud.com>
2025-09-13 00:34:58 -0300

    fixes

--- Data/UserDbContext.cs
+++ Data/UserDbContext.cs
@@ -9,7 +9,7 @@ namespace UserApi.Data
         {
         }
 
-        public DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
